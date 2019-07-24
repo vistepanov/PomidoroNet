cf55cc68c8d47c37872d7e8ec1832b7c54ee44c2
vistepanov <wstepanow@yandex.ru>
2019-07-24 15:23:40 +0300

    Folder structure changed + add new subproject

---
+++ Lync/Lync13Status.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Lync.Model;
+using PomidoroNet;
+
+namespace LyncStatus
+{
+    public class Lync13Status:IMessengerStatus
+    {
+        private LyncClient _lyncClient;
+        public Lync13Status()
+        {
+            _lyncClient = OpenClient();
+
+        }
+        public void SetStatus(MessengerStatus status)
+        {
+            try
+            {
+                //Add the availability to the contact information items to be published
+                var newInformation = new Dictionary<PublishableContactInformationType, object>
+                {
+                    {PublishableContactInformationType.Availability, GetLinqStatus(status)}
+                };
+
+                //Publish the new availability value
+                _lyncClient.Self.BeginPublishContactInformation(newInformation, PublishContactInformationCallback,
+                    null);
+
+            }
+            catch
+            {
+                //do nothing
+            }
+        }
+
+        private static object GetLinqStatus(MessengerStatus status)
+        {
+            switch (status)
+            {
+                case MessengerStatus.Free:
+                    return ContactAvailability.Free;
+                case MessengerStatus.Busy:
+                    return ContactAvailability.Busy;
+                case MessengerStatus.DoNotDisturb:
+                    return ContactAvailability.DoNotDisturb;
+                case MessengerStatus.Away:
+                    return ContactAvailability.Away;
+                default:
+                    return ContactAvailability.Free;
+            }
+        }
+
+        private LyncClient OpenClient()
+        {
+            return LyncClient.GetClient();
+        }
+        private void PublishContactInformationCallback(IAsyncResult result)
+        {
+            _lyncClient.Self.EndPublishContactInformation(result);
+        }
+
+    }
+}
---
+++ PomidoroNet/IStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace PomidoroNet
+{
+    public enum MessengerStatus
+    {
+        Free,
+        Busy,
+        DoNotDisturb,
+        Away
+    }
+    public interface IMessengerStatus
+    {
+        void SetStatus(MessengerStatus status);
+    }
+}
